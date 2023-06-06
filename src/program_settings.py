f5b472db22af27222e7aa1ffd4b7def0904d63ea
Boris Kotov <bk@webdeb.de>
2023-06-06 21:00:17 +0200

    refactor

--- src/program_settings.py
+++ src/program_settings.py
@@ -6,55 +6,55 @@ import machine
 """
 Store
 """
-CONTRAST_PATH         = "settings.contrast"
-MACHINE_FREQ_PATH     = "settings.machine_freq"
+CONTRAST_PATH = "settings.contrast"
+MACHINE_FREQ_PATH = "settings.machine_freq"
 INIT_STRUCTURE = {
-  "version": 3,
-  "settings": {
-    "contrast": 200,
-    "machine_freq": 125_000_000
-  },
+    "version": 3,
+    "settings": {
+        "contrast": 200,
+        "machine_freq": 125_000_000
+    },
 }
-
 store = Store(path="data/settings_store.json", inital_data=INIT_STRUCTURE)
 
+
 class Settings(UIListProgram):
-  title = "Settings"
+    title = "Settings"
 
-  def __init__(self, on_exit):
-    self.items = [
-      {
-        "text": ["Contrast:", self.get_contrast ],
-        "handle_change": self.handle_contrast_change
-      },
-      {
-        "text": ["Freq:", self.get_freq],
-        "handle_change": self.handle_freq_change
-      }
-    ]
+    def __init__(self, on_exit):
+        self.items = [
+            {
+                "text": ["Contrast:", self.get_contrast],
+                "handle_change": self.handle_contrast_change
+            },
+            {
+                "text": ["Freq:", self.get_freq],
+                "handle_change": self.handle_freq_change
+            }
+        ]
 
-    self.handle_button = on_exit
-    super().__init__()
+        self.handle_button = on_exit
+        super().__init__()
 
-  def get_freq(self):
-    return str(int(machine.freq()/1_000_000)) + "MHz"
+    def get_freq(self):
+        return str(int(machine.freq()/1_000_000)) + "MHz"
 
-  def handle_freq_change(self, event):
-    freq = int(machine.freq() / 1_000_000)
-    if (event == Rotary.INC):
-      freq += 1
-    elif (event == Rotary.DEC):
-      freq -= 1
+    def handle_freq_change(self, event):
+        freq = int(machine.freq() / 1_000_000)
+        if (event == Rotary.INC):
+            freq += 1
+        elif (event == Rotary.DEC):
+            freq -= 1
 
-    machine.freq(max(100, min(200, freq)) * 1_000_000)
+        machine.freq(max(100, min(200, freq)) * 1_000_000)
 
-  def get_contrast(self):
-    return str(self.display.get_contrast())
+    def get_contrast(self):
+        return str(self.display.get_contrast())
 
-  def handle_contrast_change(self, event):
-    contrast = int(self.display.get_contrast())
-    if (event == Rotary.INC):
-      contrast += 10
-    elif (event == Rotary.DEC):
-      contrast -= 10
-    self.display.set_contrast(contrast)
+    def handle_contrast_change(self, event):
+        contrast = int(self.display.get_contrast())
+        if (event == Rotary.INC):
+            contrast += 10
+        elif (event == Rotary.DEC):
+            contrast -= 10
+        self.display.set_contrast(contrast)
--- src/twofam/program.py
+++ src/twofam/program.py
@@ -1,21 +1,21 @@
 from rp2 import PIO
 
 from ui_program import (
-  UIListProgram,
-  TAP_LEFT,
-  TAP_RIGHT,
-  INC,
-  DEC,
+    UIListProgram,
+    TAP_LEFT,
+    TAP_RIGHT,
+    INC,
+    DEC,
 )
 
 from utils import (
-  freq_to_str
+    freq_to_str
 )
 
 from lib.piopwm.piopwm import (
-  PIOPWM,
-  WITH_PIN,
-  WITH_PIN_INVERTED,
+    PIOPWM,
+    WITH_PIN,
+    WITH_PIN_INVERTED,
 )
 
 import twofam.store as store
@@ -26,217 +26,221 @@ F2_PHASE = "f2_phase"
 PACKAGE = "package"
 
 PWMS = {
-  F1:       { "pin": 14, "sm": 0, "with": PACKAGE },
-  F2:       { "pin": 11, "sm": 1, "with": F2_PHASE },
-  F2_PHASE: { "pin": 13, "sm": 2, "with": PACKAGE, "inverted": True },
-  PACKAGE:  { "pin": 10, "sm": 3 },
+    F1:       {"pin": 14, "sm": 0, "with": PACKAGE},
+    F2:       {"pin": 11, "sm": 1, "with": F2_PHASE},
+    F2_PHASE: {"pin": 13, "sm": 2, "with": PACKAGE, "inverted": True},
+    PACKAGE:  {"pin": 10, "sm": 3},
 }
 
+
 class Program(UIListProgram):
-  max_freq_exp = 3 # 1 -> tick.. 2 -> 10%, 3 -> 50%
-  max_duty_exp = 3 # 1 -> tick.. 2 -> 10%, 3 -> 50%
-  
-  title = "MULTI PWM v0.1.0"
-  pwm_exp = {
-    "f1_freq": 0,
-    "f1_duty": 0,
-    "f2_freq": 0,
-    "f2_duty": 0,
-    "f2_phase_duty": 0,
-    "package_freq": 0,
-    "package_duty": 0,
-  }
+    max_freq_exp = 3  # 1 -> tick.. 2 -> 10%, 3 -> 50%
+    max_duty_exp = 3  # 1 -> tick.. 2 -> 10%, 3 -> 50%
 
-  pwms = {}
+    title = "MULTI PWM v0.1.0"
+    pwm_exp = {
+        "f1_freq": 0,
+        "f1_duty": 0,
+        "f2_freq": 0,
+        "f2_duty": 0,
+        "f2_phase_duty": 0,
+        "package_freq": 0,
+        "package_duty": 0,
+    }
 
-  def __init__(self, on_exit):
-    self.handle_button = on_exit
-    self.items = [
-      # """
-      # F1
-      # """
-      {
-        "text": [["F1:", self.get_exp("f1_freq")], self.freq_str(F1)],
-        "handle_change": lambda event: self.change_freq(F1, event),
-      },
-      {
-        "text": [["Duty:", self.get_exp("f1_duty")], self.duty_str(F1)],
-        "handle_change": lambda event: self.change_duty(F1, event),
-      },
+    pwms = {}
 
-      # """
-      # F2
-      # """
-      {
-        "text": [["F2:", self.get_exp("f2_freq")], self.freq_str(F2)],
-        "handle_change": lambda event: self.change_freq(F2, event),
-      },
-      {
-        "text": [["Duty:", self.get_exp("f2_duty")], self.duty_str(F2)],
-        "handle_change": lambda event: self.change_duty(F2, event),
-      },
-      {
-        "text": [["Phase:", self.get_exp("f2_phase_duty")], self.duty_str(F2_PHASE)],
-        "handle_change": lambda event: self.change_duty(F2_PHASE, event),
-      },
+    def __init__(self, on_exit):
+        self.handle_button = on_exit
+        self.items = [
+            # """
+            # F1
+            # """
+            {
+                "text": [["F1:", self.get_exp("f1_freq")], self.freq_str(F1)],
+                "handle_change": lambda event: self.change_freq(F1, event),
+            },
+            {
+                "text": [["Duty:", self.get_exp("f1_duty")], self.duty_str(F1)],
+                "handle_change": lambda event: self.change_duty(F1, event),
+            },
 
-      # """
-      # Package
-      # """
-      {
-        "text": [["P:", self.get_exp("package_freq")], self.freq_str(PACKAGE)],
-        "handle_change": lambda event: self.change_freq(PACKAGE, event),
-      },
-      {
-        "text": [["Duty:", self.get_exp("package_duty")], self.duty_str(PACKAGE)],
-        "handle_change": lambda event: self.change_duty(PACKAGE, event),
-      },
-    ]
+            # """
+            # F2
+            # """
+            {
+                "text": [["F2:", self.get_exp("f2_freq")], self.freq_str(F2)],
+                "handle_change": lambda event: self.change_freq(F2, event),
+            },
+            {
+                "text": [["Duty:", self.get_exp("f2_duty")], self.duty_str(F2)],
+                "handle_change": lambda event: self.change_duty(F2, event),
+            },
+            {
+                "text": [["Phase:", self.get_exp("f2_phase_duty")], self.duty_str(F2_PHASE)],
+                "handle_change": lambda event: self.change_duty(F2_PHASE, event),
+            },
 
-    PIO(0).remove_program()
+            # """
+            # Package
+            # """
+            {
+                "text": [["P:", self.get_exp("package_freq")], self.freq_str(PACKAGE)],
+                "handle_change": lambda event: self.change_freq(PACKAGE, event),
+            },
+            {
+                "text": [["Duty:", self.get_exp("package_duty")], self.duty_str(PACKAGE)],
+                "handle_change": lambda event: self.change_duty(PACKAGE, event),
+            },
+        ]
 
-    self.pwms[PACKAGE] = PIOPWM(
-      PWMS[PACKAGE]["sm"],
-      pin=PWMS[PACKAGE]["pin"]
-    )
-    self.pwms[F2_PHASE] = PIOPWM(
-      PWMS[F2_PHASE]["sm"],
-      pin=PWMS[F2_PHASE]["pin"],
-      mode=WITH_PIN_INVERTED,
-      in_pin=PWMS[PACKAGE]["pin"]
-    )
-    self.pwms[F1] = PIOPWM(
-      PWMS[F1]["sm"],
-      pin=PWMS[F1]["pin"],
-      in_pin=PWMS[PACKAGE]["pin"],
-      mode=WITH_PIN,
-    )
-    self.pwms[F2] = PIOPWM(
-      PWMS[F2]["sm"],
-      pin=PWMS[F2]["pin"],
-      mode=WITH_PIN,
-      in_pin=PWMS[F2_PHASE]["pin"]
-    )
+        PIO(0).remove_program()
 
-    self.load_params(PACKAGE)
-    # self.load_params(F2_PHASE)
-    self.load_params(F1)
-    self.load_params(F2)
+        self.pwms[PACKAGE] = PIOPWM(
+            PWMS[PACKAGE]["sm"],
+            pin=PWMS[PACKAGE]["pin"]
+        )
+        self.pwms[F2_PHASE] = PIOPWM(
+            PWMS[F2_PHASE]["sm"],
+            pin=PWMS[F2_PHASE]["pin"],
+            mode=WITH_PIN_INVERTED,
+            in_pin=PWMS[PACKAGE]["pin"]
+        )
+        self.pwms[F1] = PIOPWM(
+            PWMS[F1]["sm"],
+            pin=PWMS[F1]["pin"],
+            in_pin=PWMS[PACKAGE]["pin"],
+            mode=WITH_PIN,
+        )
+        self.pwms[F2] = PIOPWM(
+            PWMS[F2]["sm"],
+            pin=PWMS[F2]["pin"],
+            mode=WITH_PIN,
+            in_pin=PWMS[F2_PHASE]["pin"]
+        )
 
-    super().__init__()
+        self.load_params(PACKAGE)
+        # self.load_params(F2_PHASE)
+        self.load_params(F1)
+        self.load_params(F2)
 
-  def get_exp(self, exp_name):
-    return lambda: "x" + str(self.pwm_exp[exp_name] + 1)
-  def freq_str(self, pwm):
-    return lambda: freq_to_str(self.pwms[pwm].get_freq())
+        super().__init__()
 
-  def duty_str(self, pwm):
-    return lambda: self.get_duty_str(pwm)  
-  def get_duty_str(self, pwm):
-    pwm_duty_mode = store.get_duty_mode(pwm)
-    pwm = self.pwms[pwm]
+    def get_exp(self, exp_name):
+        return lambda: "x" + str(self.pwm_exp[exp_name] + 1)
 
-    if (pwm_duty_mode == store.DUTY_PERCENT):
-      return str(pwm.high_percent()) + "%"
+    def freq_str(self, pwm):
+        return lambda: freq_to_str(self.pwms[pwm].get_freq())
 
-    return str(pwm.get_high())
+    def duty_str(self, pwm):
+        return lambda: self.get_duty_str(pwm)
 
-  def render(self):
-    items_text = list(map(lambda i: i["text"], self.items))
-    self.display.render_menu(self.title, items_text, self.selected_item)
+    def get_duty_str(self, pwm):
+        pwm_duty_mode = store.get_duty_mode(pwm)
+        pwm = self.pwms[pwm]
 
-  def change_freq(self, pwm, event):
-    exp_key = pwm + "_freq"
-    if (exp_key not in self.pwm_exp):
-      self.pwm_exp[exp_key] = 0
+        if (pwm_duty_mode == store.DUTY_PERCENT):
+            return str(pwm.high_percent()) + "%"
 
-    exp = self.pwm_exp[exp_key]
+        return str(pwm.get_high())
 
-    if (event == TAP_LEFT):
-      self.pwm_exp[exp_key] = (exp - 1) % self.max_freq_exp
-    if (event == TAP_RIGHT):
-      self.pwm_exp[exp_key] = (exp + 1) % self.max_freq_exp
-    elif (event == INC):
-      self.update_period(pwm, -1, exp)
-    elif (event == DEC):
-      self.update_period(pwm, 1, exp)
+    def render(self):
+        items_text = list(map(lambda i: i["text"], self.items))
+        self.display.render_menu(self.title, items_text, self.selected_item)
 
-    self.render()
+    def change_freq(self, pwm, event):
+        exp_key = pwm + "_freq"
+        if (exp_key not in self.pwm_exp):
+            self.pwm_exp[exp_key] = 0
 
-  def change_duty(self, pwm, event):
-    exp_key = pwm + "_duty"
-    if (exp_key not in self.pwm_exp):
-      self.pwm_exp[exp_key] = 0
+        exp = self.pwm_exp[exp_key]
 
-    exp = self.pwm_exp[exp_key]
+        if (event == TAP_LEFT):
+            self.pwm_exp[exp_key] = (exp - 1) % self.max_freq_exp
+        if (event == TAP_RIGHT):
+            self.pwm_exp[exp_key] = (exp + 1) % self.max_freq_exp
+        elif (event == INC):
+            self.update_period(pwm, -1, exp)
+        elif (event == DEC):
+            self.update_period(pwm, 1, exp)
 
-    if (event == TAP_LEFT):
-      self.switch_duty_mode(pwm)
-    elif (event == TAP_RIGHT):
-      self.pwm_exp[exp_key] = (exp + 1) % self.max_duty_exp
-    elif (event == INC):
-      self.update_duty(pwm, 1, exp)
-    elif (event == DEC):
-      self.update_duty(pwm, -1, exp)
+        self.render()
 
-    self.render()
+    def change_duty(self, pwm, event):
+        exp_key = pwm + "_duty"
+        if (exp_key not in self.pwm_exp):
+            self.pwm_exp[exp_key] = 0
 
-  def switch_duty_mode(self, pwm):
-    prev_mode = str(store.get_duty_mode(pwm))
-    idx = store.DUTY_MODES.index(prev_mode)
-    new_duty_mode = store.DUTY_MODES[(idx + 1) % len(store.DUTY_MODES)]
-    store.set_duty_mode(pwm, new_duty_mode)
+        exp = self.pwm_exp[exp_key]
 
-  def update_period(self, pwm, inc, factor):
-    period = store.get_period(pwm)
-    new_period = self.get_value_by_factor(period, inc, factor)
-    self.set_period(pwm, new_period)
-  
-  def set_period(self, pwm, period):
-    high = store.get_high(pwm)
-    if (store.get_duty_mode(pwm) == store.DUTY_PERCENT):
-      high = round(store.get_duty_percent(pwm) * period)
+        if (event == TAP_LEFT):
+            self.switch_duty_mode(pwm)
+        elif (event == TAP_RIGHT):
+            self.pwm_exp[exp_key] = (exp + 1) % self.max_duty_exp
+        elif (event == INC):
+            self.update_duty(pwm, 1, exp)
+        elif (event == DEC):
+            self.update_duty(pwm, -1, exp)
 
-    low = period - high
-    store.set_params(pwm, (high, low))
+        self.render()
 
-    self.load_params(pwm)
+    def switch_duty_mode(self, pwm):
+        prev_mode = str(store.get_duty_mode(pwm))
+        idx = store.DUTY_MODES.index(prev_mode)
+        new_duty_mode = store.DUTY_MODES[(idx + 1) % len(store.DUTY_MODES)]
+        store.set_duty_mode(pwm, new_duty_mode)
 
-  def set_duty(self, pwm, high):
-    period = store.get_period(pwm)
-    low = period - high
-    store.set_low(pwm, low)
-    store.set_high(pwm, high)
-    store.reset_percent(pwm)
+    def update_period(self, pwm, inc, factor):
+        period = store.get_period(pwm)
+        new_period = self.get_value_by_factor(period, inc, factor)
+        self.set_period(pwm, new_period)
 
-    self.load_params(pwm)
+    def set_period(self, pwm, period):
+        high = store.get_high(pwm)
+        if (store.get_duty_mode(pwm) == store.DUTY_PERCENT):
+            high = round(store.get_duty_percent(pwm) * period)
 
-  def load_params(self, pwm):
-    params = store.get_params(pwm)
-    self.pwms[pwm].set_params(params)
+        low = period - high
+        store.set_params(pwm, (high, low))
 
-    """
+        self.load_params(pwm)
+
+    def set_duty(self, pwm, high):
+        period = store.get_period(pwm)
+        low = period - high
+        store.set_low(pwm, low)
+        store.set_high(pwm, high)
+        store.reset_percent(pwm)
+
+        self.load_params(pwm)
+
+    def load_params(self, pwm):
+        params = store.get_params(pwm)
+        self.pwms[pwm].set_params(params)
+
+        """
     Special case. Update F2_PHASE
     """
-    if (pwm == PACKAGE):
-      self.align_phase_to_package()
+        if (pwm == PACKAGE):
+            self.align_phase_to_package()
 
-  def align_phase_to_package(self):
-    package_high = store.get_high(PACKAGE)
-    # if phase high > package_high
-    store.set_high(F2_PHASE, min(store.get_high(F2_PHASE), package_high - 10))
-    self.set_period(F2_PHASE, package_high)
+    def align_phase_to_package(self):
+        package_high = store.get_high(PACKAGE)
+        # if phase high > package_high
+        store.set_high(F2_PHASE, min(
+            store.get_high(F2_PHASE), package_high - 10))
+        self.set_period(F2_PHASE, package_high)
 
-  def update_duty(self, pwm, inc, factor):
-    high = store.get_high(pwm)
-    new_duty = self.get_value_by_factor(high, inc, factor)
-    self.set_duty(pwm, new_duty)
+    def update_duty(self, pwm, inc, factor):
+        high = store.get_high(pwm)
+        new_duty = self.get_value_by_factor(high, inc, factor)
+        self.set_duty(pwm, new_duty)
 
-  def get_value_by_factor(self, value, inc, factor):
-    if (factor == 3):
-      return round(value + inc * value * 0.5)
-    if (factor == 2):
-      return round(value + inc * value * 0.1)
-    if (factor == 1):
-      return round(value + inc * value * 0.01)
-    return value + inc * 1
+    def get_value_by_factor(self, value, inc, factor):
+        if (factor == 3):
+            return round(value + inc * value * 0.5)
+        if (factor == 2):
+            return round(value + inc * value * 0.1)
+        if (factor == 1):
+            return round(value + inc * value * 0.01)
+        return value + inc * 1
