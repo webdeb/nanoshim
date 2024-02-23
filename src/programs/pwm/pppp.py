836b2c0642e14ddf03e535cbed8c5362d82c77e1
Boris Kotov <bk@webdeb.de>
2024-02-23 04:13:57 +0100

    remove

--- src/programs/pwm/pppp.py
+++
@@ -1,9 +0,0 @@
-from hackpwm import PWMSystem
-from lib.constants import OUT1, OUT3, OUT4
-from hackpwm.programs import PWM, PUSH_PULL, PHASE_PULSE
-
-Program = PWMSystem("4P", [
-    PUSH_PULL("PUSH PULL", pin=OUT1),
-    PWM("TESLA", pin=OUT3, wait_pin=OUT4),
-    PHASE_PULSE("TESLA PACK", pin=OUT4, wait_pin=OUT1, count_pin=OUT3),
-])
--- src/programs/pwm/twof_am.py
+++
@@ -1,9 +0,0 @@
-from hackpwm import PWMSystem
-from lib.constants import OUT1, OUT2, OUT3
-from hackpwm.programs import PWM, PHASE_PULSE
-
-Program = PWMSystem("2F AM", [
-    PWM("PACK", pin=OUT2),
-    PWM("TESLA 1", pin=OUT1, wait_pin=OUT2),
-    PHASE_PULSE("TESLA 2.", pin=OUT3, wait_pin=OUT1)
-])
